956e8ebaaa01e5c395978588c81503f76cd48e82
E-man <ebatis@outlook.com>
2017-10-18 17:27:54 +0200

    Initial commit some textures

---
+++ Assets/Background/Skysphere/Skysphere.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skysphere : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+    {
+        // Simple rotation
+        gameObject.transform.Rotate(0, Time.deltaTime * 2, Time.deltaTime);
+	}
+}
